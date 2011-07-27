c8677e95fcfcbced9e46c94e396b224a9b04a412
Jim Ingram <ingramj@gmail.com>
2011-07-27 08:26:35 -0500

    Added binary, octal, and hexadecimal integers.
    Also, comments.
    
    No support for #x+yyyyy or #x-yyyyy style literals yet, that'll be in
    the next commit.

--- read.c
+++ read.c
@@ -20,12 +20,25 @@
 
 #define SEP WS
 
+#define BIN_DIGIT '0': case '1'
+
+#define OCT_DIGIT \
+	'0': case '1': case '2': case '3': \
+	case '4': case '5': case '6': case '7'
+
 #define DIGIT \
 	'0': case '1': case '2': case '3': case '4': \
-case '5': case '6': case '7': case '8': case '9'
+	case '5': case '6': case '7': case '8': case '9'
+
+#define HEX_DIGIT \
+	'0': case '1': case '2': case '3': case '4': \
+	case '5': case '6': case '7': case '8': case '9': \
+	case 'a': case 'A': case 'b': case 'B': case 'c': case 'C': \
+	case 'd': case 'D': case 'e': case 'E': case 'f': case 'F'
 
 
-enum state { ST_START, ST_DECIMAL, ST_SIGN, ST_END };
+enum state { ST_START, ST_DECIMAL, ST_SIGNED, ST_HASH, ST_BINARY, ST_OCTAL,
+             ST_HEX, ST_END };
 
 
 rs_object rs_read(FILE *in)
@@ -48,7 +61,11 @@ rs_object rs_read(FILE *in)
 				cur_state = ST_END;
 				break;
 			case WS:
-				cur_state = ST_START;
+				/* skip whitespace */
+				break;
+			case ';':
+				/* skip comments */
+				while ((c = getc(in)) != '\n' && c != EOF) ;
 				break;
 			case DIGIT:
 				buf[off++] = c;
@@ -56,7 +73,10 @@ rs_object rs_read(FILE *in)
 				break;
 			case '+': case '-':
 				buf[off++] = c;
-				cur_state = ST_SIGN;
+				cur_state = ST_SIGNED;
+				break;
+			case '#':
+				cur_state = ST_HASH;
 				break;
 			default:
 				rs_fatal("expected whitespace or digit");
@@ -78,7 +98,7 @@ rs_object rs_read(FILE *in)
 				rs_fatal("expected digit or separator");
 			}
 			break;
-		case ST_SIGN:
+		case ST_SIGNED:
 			switch (c) {
 			case DIGIT:
 				buf[off++] = c;
@@ -86,6 +106,72 @@ rs_object rs_read(FILE *in)
 				break;
 			default:
 				rs_fatal("expected digit");
+			}
+			break;
+		case ST_HASH:
+			switch (c) {
+			case 'b': case 'B':
+				cur_state = ST_BINARY;
+				break;
+			case 'o': case 'O':
+				cur_state = ST_OCTAL;
+				break;
+			case 'd': case 'D':
+				cur_state = ST_DECIMAL;
+				break;
+			case 'h': case 'H':
+				cur_state = ST_HEX;
+				break;
+			default:
+				rs_fatal("expected radix (b, o, d, or h)");
+			}
+			break;
+		case ST_BINARY:
+			switch (c) {
+			case BIN_DIGIT:
+				buf[off++] = c;
+				cur_state = ST_BINARY;
+				break;
+			case SEP:
+				ungetc(c, in);
+				buf[off] = '\0';
+				obj = rs_fixnum_make(strtol(buf, NULL, 2));
+				cur_state = ST_END;
+				break;
+			default:
+				rs_fatal("expected binary digit or separator");
+			}
+			break;
+		case ST_OCTAL:
+			switch (c) {
+			case OCT_DIGIT:
+				buf[off++] = c;
+				cur_state = ST_OCTAL;
+				break;
+			case SEP:
+				ungetc(c, in);
+				buf[off] = '\0';
+				obj = rs_fixnum_make(strtol(buf, NULL, 8));
+				cur_state = ST_END;
+				break;
+			default:
+				rs_fatal("expected octal digit or separator");
+			}
+			break;
+		case ST_HEX:
+			switch (c) {
+			case HEX_DIGIT:
+				buf[off++] = c;
+				cur_state = ST_HEX;
+				break;
+			case SEP:
+				ungetc(c, in);
+				buf[off] = '\0';
+				obj = rs_fixnum_make(strtol(buf, NULL, 16));
+				cur_state = ST_END;
+				break;
+			default:
+				rs_fatal("expected hex digit or separator");
 			}
 			break;
 		default:
